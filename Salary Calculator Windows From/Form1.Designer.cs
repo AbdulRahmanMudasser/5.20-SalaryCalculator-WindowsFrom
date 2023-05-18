0e5189220624c81820ec987e96d54a4f9f3f7df2
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-18 15:15:22 +0500

    done

--- Salary Calculator Windows From/Form1.Designer.cs
+++ Salary Calculator Windows From/Form1.Designer.cs
@@ -28,13 +28,300 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.e1NameTextBox = new System.Windows.Forms.TextBox();
+            this.e1HoursTextBox = new System.Windows.Forms.TextBox();
+            this.e1RateTextBox = new System.Windows.Forms.TextBox();
+            this.e2NameTextBox = new System.Windows.Forms.TextBox();
+            this.e2HoursTextBox = new System.Windows.Forms.TextBox();
+            this.e2RateTextBox = new System.Windows.Forms.TextBox();
+            this.e3NameTextBox = new System.Windows.Forms.TextBox();
+            this.e3HoursTextBox = new System.Windows.Forms.TextBox();
+            this.e3RateTextBox = new System.Windows.Forms.TextBox();
+            this.e3PayTextBox = new System.Windows.Forms.TextBox();
+            this.e2PayTextBox = new System.Windows.Forms.TextBox();
+            this.e1PayTextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hours";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(410, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(410, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Hours";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(410, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Rate";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(778, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(778, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Hours";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(778, 197);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Rate";
+            // 
+            // e1NameTextBox
+            // 
+            this.e1NameTextBox.Location = new System.Drawing.Point(182, 30);
+            this.e1NameTextBox.Name = "e1NameTextBox";
+            this.e1NameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e1NameTextBox.TabIndex = 9;
+            // 
+            // e1HoursTextBox
+            // 
+            this.e1HoursTextBox.Location = new System.Drawing.Point(182, 106);
+            this.e1HoursTextBox.Name = "e1HoursTextBox";
+            this.e1HoursTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e1HoursTextBox.TabIndex = 10;
+            // 
+            // e1RateTextBox
+            // 
+            this.e1RateTextBox.Location = new System.Drawing.Point(182, 191);
+            this.e1RateTextBox.Name = "e1RateTextBox";
+            this.e1RateTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e1RateTextBox.TabIndex = 11;
+            // 
+            // e2NameTextBox
+            // 
+            this.e2NameTextBox.Location = new System.Drawing.Point(562, 33);
+            this.e2NameTextBox.Name = "e2NameTextBox";
+            this.e2NameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e2NameTextBox.TabIndex = 12;
+            // 
+            // e2HoursTextBox
+            // 
+            this.e2HoursTextBox.Location = new System.Drawing.Point(562, 109);
+            this.e2HoursTextBox.Name = "e2HoursTextBox";
+            this.e2HoursTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e2HoursTextBox.TabIndex = 13;
+            // 
+            // e2RateTextBox
+            // 
+            this.e2RateTextBox.Location = new System.Drawing.Point(562, 191);
+            this.e2RateTextBox.Name = "e2RateTextBox";
+            this.e2RateTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e2RateTextBox.TabIndex = 14;
+            // 
+            // e3NameTextBox
+            // 
+            this.e3NameTextBox.Location = new System.Drawing.Point(940, 33);
+            this.e3NameTextBox.Name = "e3NameTextBox";
+            this.e3NameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e3NameTextBox.TabIndex = 15;
+            // 
+            // e3HoursTextBox
+            // 
+            this.e3HoursTextBox.Location = new System.Drawing.Point(940, 109);
+            this.e3HoursTextBox.Name = "e3HoursTextBox";
+            this.e3HoursTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e3HoursTextBox.TabIndex = 16;
+            // 
+            // e3RateTextBox
+            // 
+            this.e3RateTextBox.Location = new System.Drawing.Point(940, 194);
+            this.e3RateTextBox.Name = "e3RateTextBox";
+            this.e3RateTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e3RateTextBox.TabIndex = 17;
+            // 
+            // e3PayTextBox
+            // 
+            this.e3PayTextBox.Location = new System.Drawing.Point(940, 282);
+            this.e3PayTextBox.Name = "e3PayTextBox";
+            this.e3PayTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e3PayTextBox.TabIndex = 23;
+            // 
+            // e2PayTextBox
+            // 
+            this.e2PayTextBox.Location = new System.Drawing.Point(562, 279);
+            this.e2PayTextBox.Name = "e2PayTextBox";
+            this.e2PayTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e2PayTextBox.TabIndex = 22;
+            // 
+            // e1PayTextBox
+            // 
+            this.e1PayTextBox.Location = new System.Drawing.Point(182, 279);
+            this.e1PayTextBox.Name = "e1PayTextBox";
+            this.e1PayTextBox.Size = new System.Drawing.Size(100, 26);
+            this.e1PayTextBox.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(778, 285);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 20);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Pay";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(410, 285);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 20);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Pay";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(28, 285);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(35, 20);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Pay";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 78);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Calculate Pay";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1071, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.e3PayTextBox);
+            this.Controls.Add(this.e2PayTextBox);
+            this.Controls.Add(this.e1PayTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.e3RateTextBox);
+            this.Controls.Add(this.e3HoursTextBox);
+            this.Controls.Add(this.e3NameTextBox);
+            this.Controls.Add(this.e2RateTextBox);
+            this.Controls.Add(this.e2HoursTextBox);
+            this.Controls.Add(this.e2NameTextBox);
+            this.Controls.Add(this.e1RateTextBox);
+            this.Controls.Add(this.e1HoursTextBox);
+            this.Controls.Add(this.e1NameTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox e1NameTextBox;
+        private System.Windows.Forms.TextBox e1HoursTextBox;
+        private System.Windows.Forms.TextBox e1RateTextBox;
+        private System.Windows.Forms.TextBox e2NameTextBox;
+        private System.Windows.Forms.TextBox e2HoursTextBox;
+        private System.Windows.Forms.TextBox e2RateTextBox;
+        private System.Windows.Forms.TextBox e3NameTextBox;
+        private System.Windows.Forms.TextBox e3HoursTextBox;
+        private System.Windows.Forms.TextBox e3RateTextBox;
+        private System.Windows.Forms.TextBox e3PayTextBox;
+        private System.Windows.Forms.TextBox e2PayTextBox;
+        private System.Windows.Forms.TextBox e1PayTextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Salary Calculator Windows From/Form1.cs
+++ Salary Calculator Windows From/Form1.cs
@@ -16,5 +16,64 @@ namespace Salary_Calculator_Windows_From
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string e1Name = e1NameTextBox.Text;
+            string e2Name = e2NameTextBox.Text;
+            string e3Name = e3NameTextBox.Text;
+
+            double e1Hours = Convert.ToDouble(e1HoursTextBox.Text);
+            double e2Hours = Convert.ToDouble(e2HoursTextBox.Text);
+            double e3Hours = Convert.ToDouble(e3HoursTextBox.Text);
+
+            double e1Rate = Convert.ToDouble(e1RateTextBox.Text);
+            double e2Rate = Convert.ToDouble(e2RateTextBox.Text);
+            double e3Rate = Convert.ToDouble(e3RateTextBox.Text);
+
+            double e1Pay, e2Pay, e3Pay;
+
+            // for e1
+            if (e1Hours >= 40)
+            {
+                e1Pay = e1Rate * 40;
+            }
+
+            else
+            {
+                e1Pay = (e1Rate * 40) + ((e1Hours - 40) * e1Rate * 1.5); 
+            }
+
+            // for e2
+            if (e2Hours >= 40)
+            {
+                e2Pay = e2Rate * 40;
+            }
+
+            else
+            {
+                e2Pay = (e2Rate * 40) + ((e2Hours - 40) * e2Rate * 1.5);
+            }
+
+            // for e3
+            if (e3Hours >= 40)
+            {
+                e3Pay = e3Rate * 40;
+            }
+
+            else
+            {
+                e3Pay = (e3Rate * 40) + ((e3Hours - 40) * e3Rate) * 1.5;
+            }
+
+            e1PayTextBox.Text = e1Pay.ToString();
+            e2PayTextBox.Text = e2Pay.ToString();
+            e3PayTextBox.Text = e3Pay.ToString();
+        }
     }
 }
